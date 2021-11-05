4f9b71c89318ab81ffa9cedd01074dd7b7f8a135
Kashif Williams <kashwill@Kashifs-MacBook-Air.local>
2021-11-05 16:24:05 -0400

    still working

--- src/main/java/com/codedifferently/assessment/corejava/part01/PredicateUtilities.java
+++ src/main/java/com/codedifferently/assessment/corejava/part01/PredicateUtilities.java
@@ -1,20 +1,26 @@
 package com.codedifferently.assessment.corejava.part01;
 
+import javax.swing.text.DefaultStyledDocument;
+
 public class PredicateUtilities {
     /**
      * @param value - the value to be evaluated
      * @return true if `value` is a multiple of 2
      */
     public Boolean isEven(Integer value) {
-        return null;
+        if ( value % 2 == 0 )
+            return true;
+        else
+           return false;
     }
+
 
     /**
      * @param value - the value to be evaluated
      * @return true if `value` is not a multiple of 2
      */
     public Boolean isOdd(Integer value) {
-        return null;
+    return null;
     }
 
     /**
@@ -22,7 +28,10 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of 3
      */
     public Boolean isMultipleOf3(Integer value) {
-        return null;
+        if ( value % 3 == 0 )
+            return true;
+        else
+            return false;
     }
 
     /**
@@ -32,14 +41,21 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of `multiple`
      */
     public Boolean isMultipleOfN(Integer value, Integer multiple) {
-        return null;
-    }
+        if( value % multiple == 0)
+            return true;
+        else
 
+            return false ;
+    }
     /**
      * @param string - the string to be evaluated
      * @return true if `string` starts with a capital letter
      */
     public Boolean startsWithCapitalLetter(String string) {
-        return null;
+        if(Character.isUpperCase(string.charAt(0)))
+
+        return true;
+        else
+            return false;
     }
 }
